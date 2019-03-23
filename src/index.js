0e7eb245ef39f5eb35699be0cab26036590f2c43
bulbyash <bulbyash@gmail.com>
2019-03-23 10:39:05 +0300

    feat: solveSudoke function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,58 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+  if (solution(matrix, 0, 0)){
+		return matrix;
+	}
 }
+
+//main function
+function solution(matrix, row, col) {
+	var cell = findEmpty(matrix, row, col);
+	if (!cell) {
+		return true;
+	}
+	row = cell[0];
+	col = cell[1];
+
+	for (var num = 1; num <= 9; num++) {
+		if (!checkRow(matrix, row, col, num)) {   
+			matrix[row][col] = num;
+			if (solution(matrix, row, col) ) {                
+				return true;
+			}  
+			matrix[row][col] = 0;
+		}
+	}
+return false;
+}
+
+//finding empty cell
+function findEmpty(matrix, row, col) {
+	for (let i = 0; i < 9; i++){
+		for (let j = 0; j < 9; j++){
+			if (matrix[i][j] == 0) {
+				return [i, j];
+            }	
+		}
+	}
+    return null;
+}
+
+//checking row
+function checkRow(matrix, row, col, num){	
+	for (var i = 0; i < 9; i++){
+		if (matrix[row][i] == num || matrix[i][col] == num){
+			return true;
+		}
+	}
+	let r = Math.floor(row - row%3);
+	let c = Math.floor(col - col%3);
+
+	for (let i = r; i < r+3; i++) {
+		for (let j = c; j < c+3; j++) {
+			if (matrix[i][j] == num) {
+				return true;
+			}
+		}
+	}
+	return false;	
+} 
